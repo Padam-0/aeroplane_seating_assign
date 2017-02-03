e72a50c2b99d1cd611027fd154843b041e823242
Peter Adam <padams@Peters-MacBook-Pro.local>
2017-02-03 12:57:19 +0000

    Add function definitions

--- seat_assign_16201859_16203177.py
+++ seat_assign_16201859_16203177.py
@@ -2,6 +2,19 @@ from sqlalchemy import create_engine
 import sys
 
 
+def organize_booking(booking_name, pas_in_booking, empty_seats_per_row,
+        empty_seats, cols, engine):
+    pass
+
+
+def find_allocation_order(number_of_pas, cols, empty_seats_per_row):
+    pass
+
+
+def write_database(engine, command, data):
+    pass
+
+
 def main():
     pass
 
